e401df6d4cb25fbe68b7e677445bcf8227293b9f
prince-ao <princead@tutanota.com>
2022-02-21 17:26:56 -0500

    bottom nav done

--- app/src/main/java/com/example/samizdat/MainActivity.kt
+++ app/src/main/java/com/example/samizdat/MainActivity.kt
@@ -4,12 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.samizdat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomnavigationbar.background = null
+        supportFragmentManager.beginTransaction().replace(R.id.framecontainer, HomeFragment()).commit()
+
+        binding.bottomnavigationbar.setOnItemSelectedListener {
+            var temp: Fragment? = null
+            when(it.itemId){
+                R.id.nHome -> temp = HomeFragment()
+                R.id.nSearch -> temp = SearchFragment()
+                R.id.nFavorites -> temp = FavFragment()
+                R.id.nBooks -> temp = BooksFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.framecontainer, temp!!).commit()
+
+            true
+        }
     }
 }
